84609aa808690ff44bd79fbf29b789bc093c6ac4
Akhilesh Singh <akhileshgkp0103@gmail.com>
2018-01-20 19:54:21 +0530

    app tast case file include

---
+++ tests/app.test.js
@@ -0,0 +1,44 @@
+
+var expect  = require("chai").expect;
+var should = require('chai').should();
+var sinon = require("sinon");
+var proxyquire =  require('proxyquire')
+
+
+describe("Server test cases", function() {
+	var mockApp, mockLogger;
+	var app, logger, server;
+	var callback = function(){};
+	beforeEach(function() {
+		console.log('running block for beforeEach');
+		app = {
+			init : callback,
+			start : callback
+		}
+
+		logger = {
+			info : callback
+		}
+		
+		server = proxyquire
+	    .noCallThru()
+	    .load('../app', {
+	        './app' : app,
+	        './logger/logger' : logger
+	       
+	    });
+
+	    mockApp = sinon.stub(app);
+	});
+
+	 describe('setting app start', function(){
+	  	it('it exits', function(){
+	  		expect(server).to.exist;
+	  	})
+
+	  	it('it object', function(){
+	  		expect(server).to.be.an('object');
+	  	})
+	 })
+
+});
